49268ef4f6e5a16a4485c995f3de33bb7ac34d62
giane-09 <gianelaamv@gmail.com>
2022-12-01 10:14:01 -0500

    final

---
+++ lib/ui/widgets/item_type_widget.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+class ItemTypeWidget extends StatelessWidget {
+
+  String text;
+  ItemTypeWidget({required this.text,});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+      margin: const EdgeInsets.only(bottom: 4, right: 12.0, top: 4.0),
+      padding: const EdgeInsets.symmetric(
+          horizontal: 14.0, vertical: 0.4),
+      decoration: BoxDecoration(
+        color: Colors.white.withOpacity(0.27),
+        borderRadius: BorderRadius.circular(10.0),
+        boxShadow: [
+          BoxShadow(
+              color: Colors.black.withOpacity(0.07),
+              offset: const Offset(4, 4),
+              blurRadius: 12.0
+          )
+        ],
+      ),
+      child: Text(
+        "Grass",
+        style: TextStyle(
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
+
+
